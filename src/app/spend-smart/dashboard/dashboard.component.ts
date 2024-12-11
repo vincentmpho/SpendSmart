50ee75b3e7a5995cdbe9a9a139e7961563d276a2
MphoM <MphoM@MP25WBGZ-NB>
2024-12-11 21:00:10 +0200

    update currency display from USD ($) to South African Rand (R)

--- src/app/spend-smart/dashboard/dashboard.component.ts
+++ src/app/spend-smart/dashboard/dashboard.component.ts
@@ -3,36 +3,38 @@ import { MatIconModule } from '@angular/material/icon';
 import { SideNavComponent } from '../side-nav/side-nav.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
- 
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [MatIconModule, SideNavComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
   //Income
   lastMonthsIncome = ['January: R1000', 'February: R1500', 'March: R1200'];
-  currentMonthIncome = 'R2000';
- 
+  currentMonthIncome = 2000;
+  
   //Expense
   lastMonthsExpense = ['January: R800', 'February: R1000', 'March: R1200'];
-  currentMonthExpense = 'R1500';
- 
-  //Todo Trans
+  currentMonthExpense = 1500;
+  
+  //Todo Transactions
   todoTransactions = [
     { description: 'Pay electricity bill' },
-    { description: 'SelfCare '},
+    { description: 'Self-care' },
     { description: 'Buy groceries' },
     { description: 'Gym' }
   ];
- 
+
   //Total
   totalCurrentMonthIncome = 2000;
   totalCurrentMonthExpense = 1500;
-  constructor(public router: Router) { }
- 
+
+  constructor(public router: Router) {}
+
+  //Navigation Methods
   onIncome() {
     this.router.navigate(['/spend-smart/income']);
   }
@@ -42,10 +44,9 @@ export class DashboardComponent {
   onTodo() {
     this.router.navigate(['/spend-smart/todo']);
   }
- 
-  //Calculate Total
+
+  //Calculate Current Month Savings
   get currentMonthSavings(): number {
     return this.totalCurrentMonthIncome - this.totalCurrentMonthExpense;
   }
- 
 }
