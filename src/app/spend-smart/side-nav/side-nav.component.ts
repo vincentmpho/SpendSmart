b882c0cb7f6484b8907521ab5b210ef0647d7a48
MphoM <MphoM@MP25WBGZ-NB>
2025-01-16 15:37:01 +0200

    remove space

--- src/app/spend-smart/side-nav/side-nav.component.ts
+++ src/app/spend-smart/side-nav/side-nav.component.ts
@@ -2,36 +2,31 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-side-nav',
   standalone: true,
   imports: [MatIconModule],
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
   isSlideOut = true;
-  constructor(private router: Router){}
+  constructor(private router: Router) {}
 
-  toggleSlideOut(): void{
-     this.isSlideOut = !this.isSlideOut;
+  toggleSlideOut(): void {
+    this.isSlideOut = !this.isSlideOut;
   }
 
-  onDash(){
+  onDash() {
     this.router.navigate(['/spend-smart/dashboard']);
-   }
-   onProfile(){
+  }
+  onProfile() {
     this.router.navigate(['/spend-smart/profile']);
-   }
-   onHistory(){
+  }
+  onHistory() {
     this.router.navigate(['/spend-smart/history']);
-   }
-   onLogout(){
+  }
+  onLogout() {
     this.router.navigate(['/spend-smart/login']);
-   }
-
+  }
 }
-
-
-
